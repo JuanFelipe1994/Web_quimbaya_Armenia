6345233278bd16029b57fd2dcaffcadd66d3bdd0
Juan Felipe Cancino Rincón <jfelipecr94@gmail.com>
2016-06-17 12:57:17 -0500

    Delete cambia_contrasena.php

--- libs/php/cambia_contrasena.php
+++
@@ -1,9 +0,0 @@
-<?php  
-	session_start();
-	require('conexion.php');
-
-	$contrasena = $_GET['contrasena'];
-	$usuario = $_GET['usuario'];
-	//cambia la contraseña actual por la nueva contraseña temporal
-	$cambio = mysql_query("UPDATE usuarios SET clave = '$contrasena' WHERE id_usuario = $usuario", $conexion);
-?>
